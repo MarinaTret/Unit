9de566fc2ade12fe422ef040c681454bea989f73
MarinaTret <jiharka88@mail.ru>
2024-03-12 13:02:42 +0300

    add test for testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,32 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import static org.testng.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CashbackHackServiceTest {
 
-class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void amountMore0() {
+        CashbackHackService service = new CashbackHackService();
 
-    @Test //покупка больше 1000
-    void remain() {
+        int actual = service.remain(2400);
+        int expected = 600;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void amount0() {
+        CashbackHackService service = new CashbackHackService();
 
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void amount1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
     }
 }
